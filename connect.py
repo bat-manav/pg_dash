1ab21e16f12f9ae7953f8c1af70e2c855effc4bd
K RAJKISHORE PATRO <68768446+bat-manav@users.noreply.github.com>
2023-01-13 11:11:00 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 import psycopg2
 def connect1():
     connection = psycopg2.connect(user="enterprisedb",
-                                                                  password="edb#123",
+                                                                  password="######",
                                                                   host="54.91.***.**",
                                                                   port="5432",
                                                                   database="postgres")
